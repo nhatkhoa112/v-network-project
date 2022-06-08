e64c552f4f88b50c588d8a2a5211bca2be31d994
nhat khoa <nhatkhoa.tin@gmail.com>
2022-06-08 08:10:25 +0700

    crete the pfofile components

---
+++ client/src/pages/profile/[id].js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react';
+
+import { useParams } from 'react-router-dom';
+
+const Profile = () => {
+  return <div className="profile"></div>;
+};
+
+export default Profile;
